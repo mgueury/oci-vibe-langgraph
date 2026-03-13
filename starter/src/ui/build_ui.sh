8d0f80ebe75603a4142c21bac65beb28a0ee948b
Marc Gueury <marc.gueury@oracle.com>
2026-03-13 17:21:11 +0100

    v3.0

--- starter/src/ui/build_ui.sh
+++ starter/src/ui/build_ui.sh
@@ -13,3 +13,43 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 build_ui
 
+# LiveLabs
+# Create a self signed certificate for the IP 
+if [ "$APIGW_HOSTNAME" = "" ]; then
+   if [ ! -f $TARGET_DIR/compute/compute/nginx_tls.conf ]; then
+      # Nginx config
+      mkdir -p $TARGET_DIR/compute/compute
+      cp nginx_tls.conf $TARGET_DIR/compute/compute/.
+      cd $TARGET_DIR/compute/compute
+      file_replace_variables nginx_tls.conf 
+
+      # IP Certificate Request      
+      cat > san.cnf << EOF     
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[dn]
+C = US
+ST = State
+L = City
+O = Organization
+CN = $BASTION_IP
+
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+IP.1 = $BASTION_IP
+EOF
+
+
+      # Generate the key and the chain      
+     openssl genrsa -out server.key 2048
+     openssl req -new -key server.key -out server.csr -config san.cnf
+     openssl x509 -req -in server.csr -signkey server.key -out server.crt -days 365 -extensions req_ext -extfile san.cnf
+   fi
+fi
